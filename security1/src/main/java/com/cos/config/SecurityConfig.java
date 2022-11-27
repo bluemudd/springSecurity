33ee05521e5a7ff9664bed29c428d54b58c1d96e
bluemudd <parkjm0817@gmail.com>
2022-11-27 15:12:48 +0900

    권한설정 어노테이션

--- security1/src/main/java/com/cos/config/SecurityConfig.java
+++ security1/src/main/java/com/cos/config/SecurityConfig.java
@@ -1,12 +1,14 @@
 package com.cos.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled=true, prePostEnabled=true)//secured 어노테이션 활성화, preAuthorize라는 어노테이션 활성화, postAuthorize라는 어노테이션 활성화
 public class SecurityConfig {
 	//해당 메서드의 리턴되는 오브젝트를 IoC로 등록해준다.
 	@Bean
--- security1/src/main/java/com/cos/controller/IndexController.java
+++ security1/src/main/java/com/cos/controller/IndexController.java
@@ -1,6 +1,9 @@
 package com.cos.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,15 +26,15 @@ public class IndexController {
 		return "index";
 	}
 	@GetMapping("/user")
-	public String user() {
+	public @ResponseBody String user() {
 		return "user";
 	}
 	@GetMapping("/admin")
-	public String admin() {
+	public @ResponseBody String admin() {
 		return "admin";
 	}
 	@GetMapping("/manager")
-	public String manager() {
+	public @ResponseBody String manager() {
 		return "manager";
 	}
 	@GetMapping("/login")
@@ -63,5 +66,15 @@ public class IndexController {
 	public String loginProc() {
 		return "loginProc";
 	}
-
+	@Secured("ROLE_ADMIN")//접근가능한 권한설정
+	@GetMapping("/info")
+	public @ResponseBody String info() {
+		return "개인정보";
+	}
+	@PreAuthorize("hasRole('ROLE_MANAGER')or hasRole('ROLE_ADMIN')")//실행되기 직전에 실행
+	//@PostAuthorize()//함수가 끝나고 난뒤에 실행
+	@GetMapping("/data")
+	public @ResponseBody String data() {
+		return "데이터정보";
+	}
 }
